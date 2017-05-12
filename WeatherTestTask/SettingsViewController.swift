dd3c7c957ea3f92f5d51c95b66f8c9fc8f101353
Admin <ruslanpopesku95@gmail.com>
2017-05-12 15:05:52 +0300

    working with sections in settings but need to fix logic

--- WeatherTestTask/SettingsViewController.swift
+++ WeatherTestTask/SettingsViewController.swift
@@ -18,7 +18,12 @@ class SettingsViewController: UITableViewController {
     
     fileprivate var weatherToGiveBack : WeatherResponse?
     fileprivate var objects = [Place]()
-    fileprivate var search = String()
+    fileprivate let lettersArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    fileprivate var sectionsArray = [Character]()
+    fileprivate var itemsInSections = [[String]]()
+    
+    var dict : [ Character : [String] ] = [:]
+    
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -44,6 +49,8 @@ class SettingsViewController: UITableViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         objects = RealmCRUD.shared.queryRealmPlacesToArray()
+        sectionsArray = getHeaders(at: objects)
+        itemsInSections = getItemInSections(sectionsArray, with: objects)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -55,13 +62,13 @@ class SettingsViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return itemsInSections[section].count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath) as! SettingsTableViewCell
         
-        cell.cityNameOutlet.text = "\(indexPath.row + 1)) " + objects[indexPath.row].address
+        cell.cityNameOutlet.text = itemsInSections[indexPath.section][indexPath.row]
         
         return cell
     }
@@ -75,17 +82,77 @@ class SettingsViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.search = objects[indexPath.row].name
         getWeather(in: objects[indexPath.row], from: tableView.cellForRow(at: indexPath) as! SettingsTableViewCell)
     }
     
 }
 
 
+//MARK: For sections
+extension SettingsViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionsArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return String(sectionsArray[section])
+    }
+    
+    fileprivate func getHeaders(at allPlaces: [Place]) -> [Character] {
+        
+        var currentSections = [Character]()
+        
+        //every letter path through
+        for letter in lettersArray {
+            
+            //every place path through
+            for place in allPlaces {
+                
+                //check if the first letter of place is an iteration letter
+                let placeNameFirstCharacter = place.name.characters[place.name.startIndex]
+                if placeNameFirstCharacter == Character(letter) {
+                    
+                    //add letter if it does not exist already in curerntheaders
+                    if !currentSections.contains( Character(letter) ) {
+                        currentSections.append( Character(letter) )
+                    }
+                }
+            }
+        }
+        return currentSections
+    }
+    
+    fileprivate func getItemInSections(_ sections: [Character], with places: [Place]) -> [ [String] ] {
+        
+        var tempItemsInSections = [ [String] ]()
+        
+        //every letter in section path through
+        for letter in sections {
+            
+            
+            //every place path through
+            for place in places {
+                
+                let placeNameFirstCharacter = place.name.characters[place.name.startIndex]
+                if placeNameFirstCharacter == letter {
+
+                    tempItemsInSections.append([place.name])
+                }
+            }
+        }
+        
+        return tempItemsInSections
+    }
+    
+}
+
+
+
 extension SettingsViewController {
     
     fileprivate func getWeather(in place: Place, from cell: SettingsTableViewCell) {
-    
+        
         cell.activityIndicator.startAnimating()
         WeatherApi.shared.getWeatherData(latitude: place.latitude, longitude: place.longitude) { weatherResponse in
             
