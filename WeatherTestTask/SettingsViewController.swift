961ba2879ca13b5b38256d4f6db7349ee080ff07
Admin <ruslanpopesku95@gmail.com>
2017-05-15 11:41:57 +0300

    Search is working for first letter in section

--- WeatherTestTask/SettingsViewController.swift
+++ WeatherTestTask/SettingsViewController.swift
@@ -12,7 +12,7 @@ protocol SettingsViewControllerDelegate {
     func doSomething(with weather: WeatherResponse)
 }
 
-class SettingsViewController: UITableViewController {
+class SettingsViewController: UITableViewController, UISearchBarDelegate {
     
     
     //MARK: delegetions
@@ -24,7 +24,7 @@ class SettingsViewController: UITableViewController {
     var objects = [Place]()
     let lettersArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     var dict = [ "" : [Place]() ]
-    
+    var filteredDict = [ "" : [Place]() ]
     
     //MARK: outlets/actions
     @IBOutlet weak var searchBookmarks: UISearchBar!
@@ -50,11 +50,15 @@ class SettingsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        searchBookmarks.delegate = self
+        
         navigationItem.title = "Settings"
         self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(title: "Delete All", style: .plain, target: self, action: #selector(deleteAllPlaces))
         
         objects = RealmCRUD.shared.queryPlacesToArray()
         dict = getSectionsAndRows(at: objects)
+        filteredDict = dict
     }
        
     override func viewWillDisappear(_ animated: Bool) {
@@ -65,16 +69,16 @@ class SettingsViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let neededKey = Array(dict.keys)[section]
-        return (dict[neededKey]?.count)!
+        let neededKey = Array(filteredDict.keys)[section]
+        return (filteredDict[neededKey]?.count)!
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath) as! SettingsTableViewCell
         
-        let neededKey = Array(dict.keys)[indexPath.section]
+        let neededKey = Array(filteredDict.keys)[indexPath.section]
         
-        cell.cityNameOutlet.text = dict[neededKey]?[indexPath.row].name
+        cell.cityNameOutlet.text = filteredDict[neededKey]?[indexPath.row].name
         
         return cell
     }
@@ -82,15 +86,15 @@ class SettingsViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             
-            let neededKey = Array(dict.keys)[indexPath.section]
-            let neededPlace = dict[neededKey]?[indexPath.row]
+            let neededKey = Array(filteredDict.keys)[indexPath.section]
+            let neededPlace = filteredDict[neededKey]?[indexPath.row]
             
-            if Array(dict.keys).count == 1 {
-                dict.removeValue(forKey: neededKey)
+            if Array(filteredDict.keys).count == 1 {
+                filteredDict.removeValue(forKey: neededKey)
                 let set : IndexSet = [indexPath.section]
                 tableView.deleteSections(set, with: .automatic)
-            } else if Array(dict.keys).count > 1 {
-                dict[neededKey]?.remove(at: indexPath.row)
+            } else if Array(filteredDict.keys).count > 1 {
+                filteredDict[neededKey]?.remove(at: indexPath.row)
                 tableView.deleteRows(at: [indexPath], with: .left)
             }
 
@@ -100,20 +104,57 @@ class SettingsViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        let neededKey = Array(dict.keys)[indexPath.section]
-        let neededPlace = dict[neededKey]?[indexPath.row]
+        let neededKey = Array(filteredDict.keys)[indexPath.section]
+        let neededPlace = filteredDict[neededKey]?[indexPath.row]
         
         getWeather(in: neededPlace!, from: tableView.cellForRow(at: indexPath) as! SettingsTableViewCell)
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return dict.count
+        return filteredDict.count
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return Array(dict.keys)[section]
+        return Array(filteredDict.keys)[section]
     }
     
+}
+
+//MARK: Search Bar delegetion
+extension SettingsViewController {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+
+        filteredDict = filterDictWith(text: searchText)
+        tableView.reloadData()
+    }
+    
+    private func filterDictWith(text: String) -> [ String : [Place] ] {
+        
+        var tempDict = [ "" : [Place]() ]
+        
+        //if text in search bar exist
+        if text != "" {
+            
+            //path through all dict keys to fill tempDict
+            for key in dict.keys {
+                
+                //if letter of searched text is the same as key of dict
+                if key == String(describing: text.characters.first!) || key.uppercased() == String( describing: text.characters.first!) {
+                    
+                    //fill tempDict with values of dict with key
+                    tempDict[key] = dict[key]
+                }
+            }
+            
+        } else {
+            tempDict = dict
+        }
+        
+        print(tempDict)
+        
+        return tempDict
+    }
 }
 
 
@@ -150,6 +191,7 @@ extension SettingsViewController {
     func deleteAllPlaces() {
         objects.removeAll()
         dict.removeAll()
+        filteredDict.removeAll()
         RealmCRUD.shared.deleteAllPlaces()
         tableView.reloadData()
     }
